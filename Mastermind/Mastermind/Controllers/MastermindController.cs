d2c557a3b2418224c1182acf6c5ca95214fa9238
quicoli <pauloquicoli@gmail.com>
2016-05-21 14:46:09 -0300

    Mastermind WebApi

---
+++ Mastermind/Mastermind/Controllers/MastermindController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Mastermind.Models;
+
+namespace Mastermind.Controllers
+{
+    public class MastermindController : ApiController
+    {
+        [HttpPost]
+        public InitialGameUserData NewGame(string nickName)
+        {
+            // iniciar jogo
+            return new InitialGameUserData();
+        }
+
+        [HttpPost]
+        public InitialGameUserData JoinGame(string nickName, string gameId)
+        {
+            // Verificar:
+            //   - gameId existe?
+            //   - Jogo está em andamento?
+            //   - Jogo já possui 2 jogadores?
+            //
+            // Se tudo ok, incluir este usuário no jogo
+            return new InitialGameUserData();
+        }
+
+        [HttpPost]
+        public GuessResult Guess(string[] colorGuess, string nickName, string gameId)
+        {
+            return new GuessResult();
+        }
+
+        [HttpGet]
+        public List<string> GamesAvailable()
+        {
+            return new List<string>();
+        }
+    }
+}
---
+++ Mastermind/Mastermind/Models/InitialGameUserData.cs
@@ -0,0 +1,11 @@
+﻿using Mastermind.Domain.Entities;
+namespace Mastermind.Models
+{
+    public class InitialGameUserData
+    {
+        public string GameId { get; set; } 
+        public Color[] AvailableColors { get; set; }
+        public int PlayerId { get; set; }
+        
+    }
+}
