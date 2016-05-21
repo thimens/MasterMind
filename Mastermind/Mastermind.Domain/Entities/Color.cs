f7aba364a25c38ee9032ee1f1768af3442a7c95b
thimens <thimens@gmail.com>
2016-05-21 14:16:45 -0300

    color

---
+++ Mastermind/Mastermind.Domain/Entities/Color.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind.Domain.Entities
+{
+    public enum Color
+    {
+        R, 
+        B,
+        G,
+        Y,
+        P,
+        O,
+        W,
+        C
+    }
+}
