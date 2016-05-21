bdde63acdeb2123f1e2026153ab461b551bb883f
thimens <thimens@gmail.com>
2016-05-21 16:46:40 -0300

    xx

--- Mastermind/Mastermind.Domain/Entities/Game.cs
+++ Mastermind/Mastermind.Domain/Entities/Game.cs
@@ -8,8 +8,26 @@ namespace Mastermind.Domain.Entities
 {
     public class Game
     {
+        public Game()
+        {
+            this.Sequence = GenerateSecretCombination(8);
+        }
+
         public string Id { get; set; }
         public bool Mutiplayer { get; set; }
         public List<Color> Sequence { get; set; }
+
+
+        private List<Color> GenerateSecretCombination(int colorCount)
+        {
+            Color[] colorTemp = new Color[colorCount];
+            Random randomNumbers = new Random();
+            for (int i = 0; i < colorCount; i++)
+            {
+                colorTemp[i] = (Color)randomNumbers.Next(0, 8);
+            }
+
+            return colorTemp.ToList();
+        }
     }
 }
--- Mastermind/Mastermind.Domain/Entities/Guess.cs
+++ Mastermind/Mastermind.Domain/Entities/Guess.cs
@@ -13,5 +13,37 @@ namespace Mastermind.Domain.Entities
         public List<Color> Sequence { get; set; }
         public int NearCount { get; set; }
         public int ExactCount { get; set; }
+
+
+        public void CheckHits(Color[] secretCombination)
+        {
+            var near = 0;
+            var exact = 0;
+
+            Color[] found = new Color[0];
+
+            for (int i = 0; i < secretCombination.Count(); i++)
+            {
+                if (secretCombination.Any(x => x == Sequence[i]))
+                {
+                    if (!found.Any(x => x == Sequence[i]))
+                    {
+                        near++;
+                        Array.Resize(ref found, near);
+                        found[near - 1] = Sequence[i];
+                    }
+                }
+
+                if (secretCombination[i] == Sequence[i])
+                {
+                    if (found.Any(x => x == Sequence[i]) && near > 0)
+                        near--;
+                    exact++;
+                }
+            }
+
+            this.ExactCount = exact;
+            this.NearCount = near;
+        }
     }
 }
